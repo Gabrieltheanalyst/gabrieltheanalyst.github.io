bf09dcd5dd29ba3863b7a5169998d7c1e6481fad
Gabriel Adebola <gabrielthedataanalyst@gmail.com>
2024-08-01 13:40:37 +0100

    complete overhaul

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,4 +255,36 @@
 
 		}
 
+		// to make contact form work.
+		var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your message, I will get back to you shortly!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+            	status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
 })(jQuery);
